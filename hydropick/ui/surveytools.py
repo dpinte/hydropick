86ce71c27ecd129dadabf48ac3d074e5d6b339e4
Bruce Klappauf <bklappauf@enthought.com>
2014-01-21 16:38:35 -0600

    pep8 on surveytools.py

--- hydropick/ui/surveytools.py
+++ hydropick/ui/surveytools.py
@@ -10,17 +10,23 @@ import numpy as np
 # ETS imports
 from enable.api import BaseTool
 from traits.api import Float, Enum, Int, Bool, Instance
-from chaco.api import  LinePlot
+from chaco.api import LinePlot
 
 #==============================================================================
 # Custom Tools
 #==============================================================================
 
+
 class LocationTool(BaseTool):
+    ''' Provides index position of cursor for selected image
+    '''
+
     image_index = Int
-    def normal_mouse_move(self,event):
+
+    def normal_mouse_move(self, event):
         self.image_index = self.component.map_index((event.x, event.y))[0]
         event.handled = False
+
 
 class TraceTool(BaseTool):
     """ Allows mouse update of impoundment boundary trace
@@ -43,14 +49,14 @@ class TraceTool(BaseTool):
 
     target_line = Instance(LinePlot)
 
-    def normal_right_down(self,event):
+    def normal_right_down(self, event):
         self.event_state = 'edit'
 
-    def edit_right_up(self,event):
+    def edit_right_up(self, event):
         self.event_state = 'normal'
         self.mouse_down = False
 
-    def edit_key_pressed(self,event):
+    def edit_key_pressed(self, event):
         # saw this in an example but it doesn't seem to do anything.
         if event.character == "Esc":
             self._reset()
@@ -65,21 +71,21 @@ class TraceTool(BaseTool):
         if diff > 1:
             start = min(current_index, self.last_index)
             end = start + diff + 1
-            xpts = [start,end]
+            xpts = [start, end]
             ypts = [self.last_y, newy]
             indices = range(*xpts)
-            ys = np.interp(indices,xpts,ypts)
+            ys = np.interp(indices, xpts, ypts)
         else:
             indices = [current_index]
             ys = [newy]
         return np.array(indices), np.array(ys)
 
     def normal_mouse_move(self, event):
-        newx, newy = self.component.map_data( (event.x, event.y))
+        newx, newy = self.component.map_data((event.x, event.y))
         self.depth = newy
 
-    def edit_mouse_move(self,event):
-        ''' Continuously change the impound line value to the current mouse pos.
+    def edit_mouse_move(self, event):
+        ''' Continuously change impound line value to the current mouse pos.
 
         While rt mouse button is down, this tracks the mouse movement to the
         right and changes the line value to the current mouse value at each
@@ -89,17 +95,15 @@ class TraceTool(BaseTool):
         '''
 
         if isinstance(self.target_line, LinePlot):
-
-            data = self.component.data
-            newx, newy = self.component.map_data( (event.x, event.y))
+            newx, newy = self.component.map_data((event.x, event.y))
             target = self.target_line
             xdata = target.index.get_data()
-            current_index = np.searchsorted(xdata,newx)
-
+            current_index = np.searchsorted(xdata, newx)
 
             if self.mouse_down:
                 ydata = target.value.get_data()
-                indices, ys = self.fill_in_missing_pts(current_index, newy, ydata)
+                indices, ys = self.fill_in_missing_pts(current_index,
+                                                       newy, ydata)
                 ydata[indices] = ys
                 target.value.set_data(ydata)
                 self.last_index = indices[-1]
@@ -108,7 +112,6 @@ class TraceTool(BaseTool):
             else:
                 # save this mouse position as reference for further moves while
                 # mouse_down is true.
-
                 self.mouse_down = True
                 self.last_index = current_index
                 self.last_y = newy
