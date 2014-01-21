1f536933e5c00d7391c7c206b044810129cb6995
wilsaj <wilson.andrew.j+github@gmail.com>
2014-01-21 17:18:08 -0600

    include missing apptools and scimath dependencies

--- hydropick/__init__.py
+++ hydropick/__init__.py
@@ -1,11 +1,13 @@
 
 __version__ = "0.1"
 __requires__ = [
+    "apptools>=4.2.0",
     "numpy>=1.6",
     "traits>=4.4",
     "enable>4.2",
     "chaco>=4.4",
     "fiona>=1.0.2",
+    "scimath>=4.1.2",
     "shapely>=1.2.17",
     "sdi",
 ]
