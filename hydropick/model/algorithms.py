abbf17081472b1b643ccb1ec522c2cabf42b2dc0
Bruce Klappauf <bklappauf@enthought.com>
2014-01-31 10:20:12 -0600

    commented out the setting CHECK_INTERFACES = 2 line.  We may want this somewhere though

--- hydropick/model/algorithms.py
+++ hydropick/model/algorithms.py
@@ -12,7 +12,7 @@ import numpy as np
 from traits.api import provides, Str, HasTraits
 
 import traits.has_traits
-traits.has_traits.CHECK_INTERFACES = 2 
+#traits.has_traits.CHECK_INTERFACES = 2 
 
 from .i_algorithm import IAlgorithm
 
