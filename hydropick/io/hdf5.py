47e353205dd781af33ced0a91e061b7ca8e2b0b6
wilsaj <wilson.andrew.j+github@gmail.com>
2014-02-11 13:05:08 -0600

    fall back to pytables 2.x API

--- hydropick/io/hdf5.py
+++ hydropick/io/hdf5.py
@@ -20,7 +20,7 @@ class HDF5Backend(object):
         line_name = data['survey_line_number']
         with self._open_file('a') as f:
             line_group = self._get_survey_line_group(f, line_name)
-            f.create_array(line_group, 'navigation_line', coords)
+            f.createArray(line_group, 'navigation_line', coords)
             f.flush()
 
         self._write_freq_dicts(line_name, data['frequencies'])
@@ -54,26 +54,26 @@ class HDF5Backend(object):
         frequencies_group = self._get_frequencies_group(f, line_name)
         frequency_label = 'khz_' + str(khz).replace('.', '_')
         try:
-            frequency_group = f.get_node(frequencies_group, frequency_label)
+            frequency_group = f.getNode(frequencies_group, frequency_label)
         except tables.NoSuchNodeError:
-            frequency_group = f.create_group(frequencies_group, frequency_label)
+            frequency_group = f.createGroup(frequencies_group, frequency_label)
         return frequency_group
 
     def _get_frequencies_group(self, f, line_name):
         """returns the group for the collection of frequency data for a survey line"""
         survey_line_group = self._get_survey_line_group(f, line_name)
         try:
-            frequency_group = f.get_node(survey_line_group, 'frequencies')
+            frequency_group = f.getNode(survey_line_group, 'frequencies')
         except tables.NoSuchNodeError:
-            frequency_group = f.create_group(survey_line_group, 'frequencies')
+            frequency_group = f.createGroup(survey_line_group, 'frequencies')
         return frequency_group
 
     def _get_survey_lines_group(self, f):
         """returns the group for the collection of survey_lines - creating it if necessary"""
         try:
-            survey_lines_group = f.get_node('/survey_lines')
+            survey_lines_group = f.getNode('/survey_lines')
         except tables.NoSuchNodeError:
-            survey_lines_group = f.create_group(f.root, 'survey_lines')
+            survey_lines_group = f.createGroup(f.root, 'survey_lines')
         return survey_lines_group
 
     def _get_survey_line_group(self, f, line_name):
@@ -81,9 +81,9 @@ class HDF5Backend(object):
         survey_lines = self._get_survey_lines_group(f)
         group_label = 'line_' + line_name
         try:
-            line_group = f.get_node(survey_lines, group_label)
+            line_group = f.getNode(survey_lines, group_label)
         except tables.NoSuchNodeError:
-            line_group = f.create_group(survey_lines, group_label)
+            line_group = f.createGroup(survey_lines, group_label)
         return line_group
 
     @contextlib.contextmanager
@@ -91,7 +91,7 @@ class HDF5Backend(object):
         """context manager that opens a file and also checks that the hydropick
         version number is correct
         """
-        with tables.open_file(self.filepath, mode) as f:
+        with tables.openFile(self.filepath, mode) as f:
             if len(f.listNodes('/')) == 0:
                 f.root._v_attrs.version = self.hydropick_format_version
             if not hasattr(f.root._v_attrs, 'version') or f.root._v_attrs.version != self.hydropick_format_version:
@@ -108,5 +108,5 @@ class HDF5Backend(object):
                 khz = freq_dict.pop('kHz')
                 freq_group = self._get_frequency_group(f, line_name, khz)
                 for key, value in freq_dict.iteritems():
-                    f.create_array(freq_group, key, value)
+                    f.createArray(freq_group, key, value)
             f.flush()
